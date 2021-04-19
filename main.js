1f80cb4360dc939eaf3b005f27f53230a73a0be2
leopoldonanti <leonanti21@gmail.com>
2021-04-19 17:09:16 +0200

    esercizio corretto e concluso

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ $(document).ready(function(){
 
   $(".header-right > a").click(function(){
     $(".hamburger-menu").show();
-  });
+  })
 
   $(".hamburger-menu  a.close").click(function(){
     $(".hamburger-menu").hide();
-  });
+  })
 
+})
