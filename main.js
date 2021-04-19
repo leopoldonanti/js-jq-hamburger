6f96be642cb36bd7618c0223ace43c0605f8e63d
leopoldonanti <leonanti21@gmail.com>
2021-04-19 17:00:16 +0200

    primo esercizio js-jquery finito

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  /* var menuOpened = $(".header-right > a");
+
+  var menuClosed = $(".hamburger-menu  a.close");
+
+  var listaMenu = $(".hamburger-menu"); */
+
+  
+  /* richiamo per le funzioni direttamente le classi del css */
+
+  $(".header-right > a").click(function(){
+    $(".hamburger-menu").show();
+  });
+
+  $(".hamburger-menu  a.close").click(function(){
+    $(".hamburger-menu").hide();
+  });
+
